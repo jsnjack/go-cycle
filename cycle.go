47b624e9f6e9f7d30d3d1ec34caf11dbd3fb8c97
Yauhen Shulitski <jsnjack@gmail.com>
2018-02-21 23:08:49 +0100

    :p: Get data from speed sensor

--- cycle.go
+++ cycle.go
@@ -4,11 +4,14 @@ import (
 	"encoding/binary"
 	"fmt"
 	"log"
-	"strings"
+	"os"
+	"sync"
 
 	"github.com/paypal/gatt"
 	"github.com/paypal/gatt/examples/option"
 )
+
+// List of services https://www.bluetooth.com/specifications/gatt/services
 
 // MOOVHR is the ID of a pecific MOOH HR device
 const MOOVHR = "CC:78:AB:26:B2:73"
@@ -16,7 +19,9 @@ const MOOVHR = "CC:78:AB:26:B2:73"
 // SPEEDSENSOR is the ID of powertap spd 53292
 const SPEEDSENSOR = "E7:C6:C3:FC:FC:97"
 
-var done = make(chan struct{})
+var connected int
+
+var wg sync.WaitGroup
 
 func onStateChanged(d gatt.Device, s gatt.State) {
 	fmt.Println("State:", s)
@@ -33,38 +38,52 @@ func onStateChanged(d gatt.Device, s gatt.State) {
 func onPeriphDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
 	// Move HR
 	// https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.heart_rate.xml
-	if strings.ToUpper(p.ID()) != MOOVHR {
+	switch p.ID() {
+	case MOOVHR, SPEEDSENSOR:
+		fmt.Printf("Found %s\n", p.Name())
+		p.Device().Connect(p)
+		connected = connected + 1
+	default:
 		return
 	}
 
 	// Stop scanning once we've got the peripheral we're looking for.
-	p.Device().StopScanning()
-
-	fmt.Printf("Found %s\n", p.Name())
-	p.Device().Connect(p)
+	if connected == 2 {
+		p.Device().StopScanning()
+	}
 }
 
 func onPeriphConnected(p gatt.Peripheral, err error) {
-	fmt.Println("Connected")
-	defer p.Device().CancelConnection(p)
+	fmt.Printf("Connected %s\n", p.Name())
 
 	if err := p.SetMTU(500); err != nil {
 		fmt.Printf("Failed to set MTU, err: %s\n", err)
 	}
 
-	service, err := GetService(p, "Heart Rate")
-	if err != nil {
-		fmt.Println(err)
-		return
+	switch p.ID() {
+	case MOOVHR:
+		go getHRData(p)
+	case SPEEDSENSOR:
+		go getSpeedData(p)
 	}
-	fmt.Printf("Service %s found\n", service.Name())
+}
 
-	ch, err := GetCharacteristic(p, service, "Heart Rate Measurement")
+func getHRData(p gatt.Peripheral) {
+	logger := log.New(os.Stdout, "HR ", log.Lmicroseconds|log.Lshortfile)
+	defer p.Device().CancelConnection(p)
+	service, err := GetService(p, gatt.UUID16(0x180d))
 	if err != nil {
-		fmt.Println(err)
+		logger.Println(err)
 		return
 	}
-	fmt.Printf("Characteristic %s found\n", ch.Name())
+	logger.Printf("Service %s found\n", service.Name())
+
+	ch, err := GetCharacteristic(p, service, gatt.UUID16(0x2a37))
+	if err != nil {
+		logger.Println(err)
+		return
+	}
+	logger.Printf("Characteristic %s found\n", ch.Name())
 
 	p.DiscoverDescriptors(nil, ch)
 
@@ -74,50 +93,78 @@ func onPeriphConnected(p gatt.Peripheral, err error) {
 			resultCh <- err.Error()
 		}
 		heartRate := binary.LittleEndian.Uint16(append([]byte(data[1:2]), []byte{0}...))
-		fmt.Printf("HR: %d\n", heartRate)
+		logger.Printf("BPM: %d: data %v\n", heartRate, data)
+	})
+	<-resultCh
+}
+
+func getSpeedData(p gatt.Peripheral) {
+	logger := log.New(os.Stdout, "SP ", log.Lmicroseconds|log.Lshortfile)
+	defer p.Device().CancelConnection(p)
+	service, err := GetService(p, gatt.UUID16(0x1816))
+	if err != nil {
+		logger.Println(err)
+		return
+	}
+	logger.Printf("Service %s found\n", service.Name())
+
+	ch, err := GetCharacteristic(p, service, gatt.UUID16(0x2A5B))
+	if err != nil {
+		logger.Println(err)
+		return
+	}
+	logger.Printf("Characteristic %s found\n", ch.Name())
+
+	p.DiscoverDescriptors(nil, ch)
+
+	resultCh := make(chan string)
+	p.SetNotifyValue(ch, func(ch *gatt.Characteristic, data []byte, err error) {
+		if err != nil {
+			resultCh <- err.Error()
+		}
+		offset := 1
+
+		revolutions := binary.LittleEndian.Uint32(append([]byte(data[offset:])))
+		offset += 4
+		eventTime := binary.LittleEndian.Uint16(append([]byte(data[offset:])))
+		logger.Printf("Revolutions: %d, Time: %fs: data %v\n", revolutions, float32(eventTime)/1024, data)
 	})
 	<-resultCh
 }
 
 func onPeriphDisconnected(p gatt.Peripheral, err error) {
-	fmt.Println("Disconnected")
-	close(done)
+	wg.Done()
+	fmt.Printf("Disconnected %s\n", p.Name())
 }
 
 // GetService returns service with specified name
-func GetService(p gatt.Peripheral, name string) (*gatt.Service, error) {
+func GetService(p gatt.Peripheral, uuid gatt.UUID) (*gatt.Service, error) {
 	services, err := p.DiscoverServices(nil)
 	if err != nil {
 		fmt.Printf("Failed to discover services, err: %s\n", err)
 		return nil, err
 	}
 	for _, item := range services {
-		if item.Name() == name {
+		if item.UUID().Equal(uuid) {
 			return item, nil
 		}
 	}
-	return nil, fmt.Errorf("Service %s not found", name)
+	return nil, fmt.Errorf("Service %s not found", uuid.String())
 }
 
 // GetCharacteristic returns characteristics with specified name
-func GetCharacteristic(p gatt.Peripheral, service *gatt.Service, name string) (*gatt.Characteristic, error) {
+func GetCharacteristic(p gatt.Peripheral, service *gatt.Service, uuid gatt.UUID) (*gatt.Characteristic, error) {
 	chs, err := p.DiscoverCharacteristics(nil, service)
 	if err != nil {
 		fmt.Printf("Failed to discover characteristics, err: %s\n", err)
 		return nil, err
 	}
 	for _, item := range chs {
-		if item.Name() == name {
+		if item.UUID().Equal(uuid) {
 			return item, nil
 		}
 	}
-	return nil, fmt.Errorf("Characteristic %s not found", name)
-}
-
-// HRHandler handles heart rate data
-func HRHandler(ch *gatt.Characteristic, data []byte, err error) {
-	heartRate := binary.LittleEndian.Uint16(append([]byte(data[1:2]), []byte{0}...))
-	fmt.Printf("HR: %d\n", heartRate)
+	return nil, fmt.Errorf("Characteristic %s not found", uuid.String())
 }
 
 func main() {
@@ -134,7 +181,8 @@ func main() {
 		gatt.PeripheralDisconnected(onPeriphDisconnected),
 	)
 
+	wg.Add(2)
 	d.Init(onStateChanged)
-	<-done
+	wg.Wait()
 	fmt.Println("Done")
 }
