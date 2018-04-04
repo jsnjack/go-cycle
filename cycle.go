33110d3be204d42a43fe6a806c04790fcb28458c
Yauhen Shulitski <jsnjack@gmail.com>
2018-04-04 09:42:24 +0300

    :p: Do not use bt device in debug mode

--- cycle.go
+++ cycle.go
@@ -40,18 +40,20 @@ func main() {
 	}()
 
 	// Bluetooth section
-	d, err := gatt.NewDevice(option.DefaultClientOptions...)
-	if err != nil {
-		Logger.Fatalf("Failed to open device, err: %s\n", err)
-		return
+	if !*debugFlag {
+		d, err := gatt.NewDevice(option.DefaultClientOptions...)
+		if err != nil {
+			Logger.Fatalf("Failed to open device, err: %s\n", err)
+			return
+		}
+		// Register bluetooth handlers
+		d.Handle(
+			gatt.PeripheralDiscovered(onPeriphDiscovered),
+			gatt.PeripheralConnected(onPeriphConnected),
+			gatt.PeripheralDisconnected(onPeriphDisconnected),
+		)
+		d.Init(onStateChanged)
 	}
-	// Register bluetooth handlers
-	d.Handle(
-		gatt.PeripheralDiscovered(onPeriphDiscovered),
-		gatt.PeripheralConnected(onPeriphConnected),
-		gatt.PeripheralDisconnected(onPeriphDisconnected),
-	)
-	d.Init(onStateChanged)
 
 	select {}
 }
